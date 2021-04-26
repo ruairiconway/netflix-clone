e110db1020d14d75440147c577d029c40e29de6d
Ruairi Conway <hi@ruairiconway.com>
2021-04-26 17:19:22 -0400

    ; removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import Home from './pages/home';
+import React from 'react'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import Home from './pages/home'
 
 export function App() {
   return (
@@ -21,5 +21,5 @@ export function App() {
             </Route>
         </Switch>
     </Router>
-  );
+  )
 }
