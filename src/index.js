33f308b2df7f7b52e0956d1f6a0e322461a00825
Ruairi Conway <hi@ruairiconway.com>
2021-05-19 18:32:06 -0400

    implemented firebase config
    API protected via Google restrictions

--- src/index.js
+++ src/index.js
@@ -2,11 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { GlobalStyles } from './global-styles'
 import { App } from './App'
+import { FirebaseContext } from './context/firebase'
+
+// import { seedDatabase } from './seed'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDd2s6sa3CutMfupTTQi3EbVrAS24siBxQ",
+    authDomain: "ruairiconway-notflix.firebaseapp.com",
+    databaseURL: "https://ruairiconway-notflix-default-rtdb.firebaseio.com",
+    projectId: "ruairiconway-notflix",
+    storageBucket: "ruairiconway-notflix.appspot.com",
+    messagingSenderId: "321275710894",
+    appId: "1:321275710894:web:24bd25e742536a52820c3f"
+}
+
+window.firebase.initializeApp(firebaseConfig)
+
+// const firebase = window.firebase.initializeApp(firebaseConfig)
+// seedDatabase(firebase)
 
 ReactDOM.render(
-    <>
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
         <GlobalStyles />
         <App />
-    </>,
+    </FirebaseContext.Provider>, 
     document.getElementById('root')
 )
